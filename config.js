323471aad20d8a362989860d7fb6badaea3d2fc1
dungchoanhnua1997 <114792130+dungchoanhnua1997@users.noreply.github.com>
2022-10-02 21:14:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
-    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
-    btnIntro: 'OK',
-    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
-    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
-    btnYes: 'Có, em thật sự thích anh 💖💖💖',
-    btnNo: '💔 Anh mơ à ! 💔 ',
-    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    introTitle: '🖐🖐🖐 He Lu Bé Miu ! 🖐🖐🖐',
+    introDesc: `Gâu có điều này muốn hỏi Bé, Bé nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OK Bull Gâu',
+    title: 'Bé có yêu Gâu nhiều không Bé ? 😙',
+    desc: 'Bé không trả lời hoặc thoát ra, tức là Bé yêu Gâu nhiều đó nha :> ',
+    btnYes: 'Có, Bé yêu Gâu nhiều lắm lun 💖💖💖',
+    btnNo: '💔 Gâu mơ à ! 💔 ',
+    question:'Nói cho Gâu biết đi vì sao Bé lại yêu Gâu dị 😊😊',
     reasonPlaceholder: 'Lý do',
     btnReply: 'Gởi ! 💌💌',
-    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
-    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
-    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
-    btnAccept: 'Ok lun 💖',
-    messLink: 'https://m.me/PhungQuocDinh'
-}
+    reply: 'Tại vì Gâu đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Gâu biết mà, Gâu cũng yêu Bé lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Gâu có điều muốn nhắn nhủ tới Bé là..... 💬💬',
+    btnAccept: 'Cảm ơn Bé đã yêu và ở bên cạnh Gâu, Yêu Bé rất nhiều 💖',
+}    
+
